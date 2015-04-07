12a6857d499a21f37cc45a3f2890d4fd66feed9a
c2vasu <c2vasu@yahoo.co.in>
2015-04-07 11:49:53 -0700

    Dispatcher Servlet added

---
+++ src/main/java/ca/spring/mvc/push/controller/NotificationController.java
@@ -0,0 +1,17 @@
+package ca.spring.mvc.push.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author SrinivasaRao
+ *
+ */
+@Controller
+public class NotificationController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "/WEB-INF/jsp/index.jsp";
+	}
+}
