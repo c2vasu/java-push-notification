f3390c2cbf7cfad86f7fdb9d692fe5603b952c28
c2vasu <c2vasu@yahoo.co.in>
2015-04-07 12:16:40 -0700

    sendMessage method added

--- src/main/java/ca/spring/mvc/push/controller/NotificationController.java
+++ src/main/java/ca/spring/mvc/push/controller/NotificationController.java
@@ -1,7 +1,12 @@
 package ca.spring.mvc.push.controller;
 
+import java.util.Random;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author SrinivasaRao
@@ -9,9 +14,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class NotificationController {
-
+	/**
+	 * For index page
+	 * @return view
+	 */
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
 	}
+	
+	/**
+	 * Push Notification
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping("/push")
+    public @ResponseBody String sendMessage(HttpServletResponse response) {
+            Random r = new Random();
+            response.setContentType("text/event-stream");
+            try {
+                    Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                    e.printStackTrace();
+            }   
+            return "data:Testing 1,2,3" + r.nextInt() +"\n\n";
+    }
 }
